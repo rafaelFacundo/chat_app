a6f765643975ecce29a550e85e3779c6fe44af8c
rafaelFacundo <rafaelfacundo63@gmail.com>
2024-04-25 20:55:18 -0300

    Login screen design done

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,12 +2,14 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
 import SignUpScreen from './src/screens/SignUpScreen';
+import LoginScreen from './src/screens/loginScreen';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SignUpScreen />} />
+        <Route path="/" element={<LoginScreen />} />
+        <Route path="/signup" element={<SignUpScreen />} />
       </Routes>
     </Router>
   );
